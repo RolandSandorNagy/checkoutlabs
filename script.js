99ff194403b76fbcbff8b4d527e5ff33d50dc547
RolandSandorNagy <roland.s.nagy@gmail.com>
2026-08-13 21:06:54 +0200

    Balance scroll reveal timing

--- script.js
+++ script.js
@@ -529,7 +529,7 @@ if (form && statusEl) {
         io.unobserve(entry.target);
       });
     },
-    { threshold: 0.04, rootMargin: "0px 0px 12% 0px" }
+    { threshold: 0.08, rootMargin: "0px 0px 4% 0px" }
   );
 
   els.forEach(el => io.observe(el));
